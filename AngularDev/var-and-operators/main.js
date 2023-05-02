19ae99646c1d2cf3b1b81c0023c68eeb7d6d1921
Nathan Lima <nathanls.dev@gmail.com>
2023-05-02 15:52:56 -0300

    fuel consumption

--- AngularDev/var-and-operators/main.js
+++ AngularDev/var-and-operators/main.js
@@ -1,11 +1,34 @@
-const fuelValue = 6.85;
+const gasValue = 6.85;
+const ethanolValue = 5.79;
 const KMPerLiter = 10;
 const KMDistance = 275;
 
+const fuelType = "ethanol"; //change type of fuel here! ethanol/gas
 const litersNeeded = KMDistance / KMPerLiter;
-const amountSpent = litersNeeded * fuelValue;
-const amountSpentFix = Number(amountSpent.toFixed(2));
 
-console.log(
-  `It takes $ ${amountSpentFix} dollars worth of fuel to travel ${KMDistance} Km away.`
-);
+function fuelConsumption() {
+  if (fuelType === "gas") {
+    const fuelValue = gasValue;
+    const amount = amountSpent(fuelValue);
+    return outputMSG(amount);
+  }
+
+  const fuelValue = ethanolValue;
+  const amount = amountSpent(fuelValue);
+  return outputMSG(amount);
+}
+
+const amountSpent = (fuelValue) => {
+  const amountSpent = litersNeeded * fuelValue;
+  const amountSpentFix = Number(amountSpent.toFixed(2));
+
+  return amountSpentFix;
+};
+
+const outputMSG = (amountSpentFix) => {
+  console.log(
+    `It takes $ ${amountSpentFix} dollars worth of fuel to travel ${KMDistance} Km away.`
+  );
+};
+
+fuelConsumption();
